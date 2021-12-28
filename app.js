9c0de50204c10bd6c46fa48e1a53f328e047a599
pydevjeong <njh3421@gmail.com>
2021-12-29 00:14:34 +0900

    로그인API생성/ body-parser install

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 
 //모듈
 const express=require('express')
+const bodyParser=require("body-parser")
+
 const app=express();
 
 
@@ -12,7 +14,9 @@ const home=require("./src/routes/home")
 app.set("views","./src/views");
 app.set("view engine","ejs");
 app.use(express.static(`${__dirname}/src/public`))
-
+app.use(bodyParser.json());
+//URL을 통해 전달되는 한글 ,공백 등과같은 문자가 포함될 경우 제대로 인식되지 않는 문제 해결
+app.use(bodyParser.urlencoded({extended:true}))
 
 app.use('/',home);
 //use -> 미들웨어를 등록해주는 메소드
--- src/routes/home/home.ctrl.js
+++ src/routes/home/home.ctrl.js
@@ -1,16 +1,22 @@
 "use strict";
 
-const hello = (req,res) => {
-  res.render("home/index")
+const output={
+  home: (req,res) => {
+    res.render("home/index")
+  },
+  login: (req,res) => {
+    res.render("home/login");
+  }
 }
 
-const login=(req,res) => {
-  res.render("home/login");
+const process={
+  login:(req,res) => {
+    console.log(req.body);
+  }
 }
 
 module.exports={
-  hello,
-  login
+  output,process
 };
 //여기서 object는 key와 value가 한쌍인데 이렇게 쓰면 오류가 왜 안날까?
 //이유는 key를 하나만 썼을 때는 object에서 key:key로 저장을 해준다.
