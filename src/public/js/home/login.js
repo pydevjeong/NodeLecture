a51d3d6bef3c86f1650bd1911ad136e45e5deebe
pydevjeong <njh3421@gmail.com>
2021-12-30 19:40:06 +0900

    If login sucessed go to root page

--- src/public/js/home/login.js
+++ src/public/js/home/login.js
@@ -20,5 +20,17 @@ function login() {
     body:JSON.stringify(req),
   })
   .then((res)=>res.json())
-  .then((res)=>console.log(res))
+  .then((res)=>{
+    if(res.success){
+      location.href="/";
+    }else{
+      console.log( res.msg);
+    }
+  })
+  .catch((err)=>{
+    console.error(new Error("로그인중 에러 발생"))
+    //라우팅되는 서버가 없거나 그럴때 에러발생을 잡아준다.
+    //new Error를 쓰면 Error: "로그인중 에러 발생",
+    //쓰지않으면 그냥 "로그인중 에러 발생" 이라고 출력됨
+  })
 }
